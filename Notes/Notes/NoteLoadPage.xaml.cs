66cc2ae68572d72208f20d1dc79298a9f297afc3
sergfedoro@mail.ru <sergfedoro@mail.ru>
2020-01-31 19:32:31 +0300

    loading menu bugfix

--- Notes/Notes/NoteLoadPage.xaml.cs
+++ Notes/Notes/NoteLoadPage.xaml.cs
@@ -53,6 +53,7 @@ namespace Notes
 
         private void MyListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
+            ToolbarItems.Clear();
             ToolbarItems.Add(new ToolbarItem { Text = "Delete" });
             ToolbarItems.Add(new ToolbarItem { Text = "Open" });
             ToolbarItems[0].Clicked += Delete_Clicked; ToolbarItems[1].Clicked += Open_Clicked;
